20063bd74718e54f9f7232e8baa3eba42a0dc5c8
Dima Gudkov <d.o.gudkov@gmail.com>
2020-07-14 22:17:53 +0300

    start

---
+++ gulpfile.js
@@ -0,0 +1,27 @@
+let gulp = require('gulp');
+let sass = require('gulp-sass');
+let concat = require('gulp-concat');
+let uglify = require('gulp-uglify');
+let cleancss = require('gulp-clean-css');
+let rename = require('gulp-rename');
+let autoprefixer = require('gulp-autoprefixer');
+let notify = require("gulp-notify");
+
+
+gulp.task('styles', function() {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass({ outputStyle: 'expanded' }).on("error", notify.onError()))
+        .pipe(rename({ suffix: '.min', prefix : '' }))
+        .pipe(autoprefixer(['last 15 versions']))
+        .pipe(cleancss( {level: { 1: { specialComments: 0 } } }))
+        .pipe(gulp.dest('public/assets/css'))
+});
+
+gulp.task('js', function() {
+    return gulp.src([
+        'src/js/app.js',
+    ])
+    .pipe(concat('scripts.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('public/assets/js'))
+});
---
+++ src/js/app.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function(event) {
+    console.info('hello!');
+});
